7732960e04f62b5d7e56065c5bfc91ee8800087b
Duc Nguyen <ducnv81@fpt.com>
2024-05-27 17:34:00 +0700

    fix

--- js/stringvalue.js
+++ js/stringvalue.js
@@ -2,4 +2,4 @@ const ZALO_PERMISSION_URL = "https://oauth.zaloapp.com/v4/permission"; // Social
 const ZALO_ACCESS_TOKEN_URL = "https://oauth.zaloapp.com/v4/access_token"; // Social API
 
 const APP_ID = "154765535848006805"; // FILL your Application ID here!!!
-const APP_AUTH_URL = window.location.href.substring(0, window.location.href.lastIndexOf("/")) + "/auth.html"; // URL to auth.html
+const APP_AUTH_URL = window.location.href.substring(0, window.location.href.lastIndexOf("/")) + "/success"; // URL to auth.html
